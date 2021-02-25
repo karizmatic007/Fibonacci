f70d59868ff9ed001fce71c94e0a7b69154d032c
karizmatic007 <nitishborawar@gmail.com>
2021-02-25 23:31:34 +0530

    Add files via upload
    Steps to Develop Code
    
    ->Create a C# class file in visual studio with aplication name as Fibonacce
    ->Write the code in the class file
    ->Build the project
    ->->As mentioned please provide number till Fibonacci series is needed.
     
    Unit test results
    
    -------Fibonacci Series ---------
    Enter the number of digits you want to display
    User Input:- 8
    Output:-  0, 1, 1, 2, 3, 5, 8, 13

---
+++ Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Fibonacci
+{
+	public static void Main(string[] args)
+	{
+		 Console.WriteLine("-------Fibonacci Series ---------");
+		 Console.WriteLine("Enter the number of digits you want to display");
+		 int number = int.Parse(Console.ReadLine());              
+		  int preNum = 0;
+		  int nextNum = 1;
+		 
+		  for (int i = 1; i <= number; ++i)
+		  {
+		  Console.Write("{0}" + (i < number ? ", " : ""), preNum);
+		  int sum = preNum + nextNum;
+		  preNum = nextNum;
+		  nextNum = sum;
+		  }
+	}
+}
